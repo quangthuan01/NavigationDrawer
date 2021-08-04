7ca27d0f4beca894c114259a354bad14bfc8ac45
quangthuan01 <thuannqIt01@gmail.com>
2021-08-04 09:06:50 +0700

    NavigationDrawer,MoneyManager

--- app/src/main/java/com/example/navigationdrawer/fragment/ThuFragment.java
+++ app/src/main/java/com/example/navigationdrawer/fragment/ThuFragment.java
@@ -2,51 +2,61 @@ package com.example.navigationdrawer.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.PopupMenu;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.navigationdrawer.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+import me.ibrahimsn.lib.SmoothBottomBar;
 
 
 public class ThuFragment extends Fragment {
 
-    private BottomNavigationView bottomNavigationView;
+    private SmoothBottomBar smoothBottomBar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_thu, container, false);
         //this line hide Actionbar
-        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        bottomNavigationView = view.findViewById(R.id.bottomNav);
+//        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        smoothBottomBar = view.findViewById(R.id.bottomNav);
         //set fragment default
-        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.containerThu, new MoneyFragment()).commit();
-        bottomNavigationView.setSelectedItemId(R.id.nav_money);
-        //set oclick tung item ->> fragment
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        replace(new KhoanThu_ThuFragment());
+        smoothBottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment = null;
-                switch (item.getItemId()) {
-                    case R.id.nav_home:
-                        fragment = new KhoanThu_ThuFragment();
+            public void onItemSelect(int i) {
+                switch (i) {
+                    case 0:
+                        replace(new KhoanThu_ThuFragment());
                         break;
-                    case R.id.nav_work:
-                        fragment = new LoaiThu_ThuFragment();
+                    case 1:
+                        replace(new LoaiThu_ThuFragment());
                         break;
-                    case R.id.nav_money:
-                        fragment = new MoneyFragment();
+                    case 2:
+                        replace(new MoneyFragment());
                         break;
                 }
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.containerThu, fragment).commit();
-                return true;
             }
         });
         return view;
     }
+
+    private void replace(Fragment fragment) {
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.containerThu, fragment);
+        transaction.commit();
+    }
+
 }
